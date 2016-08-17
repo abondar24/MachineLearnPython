b5a4eeaeb90408621710b8b364918167ceff0f03
abondar <abondar1992@gmail.com>
2016-08-17 18:46:32 +0100

    readme upd2 and script for non-anaconda usage

---
+++ py_setup_dl.sh
@@ -0,0 +1,14 @@
+#!usr/bin/sh
+
+# if you want to run machine learing staff without anaconda 
+# you need to install all these dependencies
+
+sudo apt-get install python3-numpy python3-scipy python3-matplotlib ipython3 ipython3-notebook python3-pandas python3-nose  libatlas-dev libatlas3gf-base graphviz
+
+sudo pip3  install sympy
+
+sudo pip3  install scikit-learn
+
+sudo pip3 install pydotplus
+
+sudo pip3 install theano
